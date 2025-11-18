c4ec65d9469287285d3c12ee8ffb6e08688432a6
ShuaiHao <e1221805@u.nus.edu>
2025-11-18 11:41:25 +0800

    fix前端路由

--- demo/frontend/vite.config.ts
+++ demo/frontend/vite.config.ts
@@ -23,7 +23,7 @@ import relay from 'vite-plugin-relay';
 import {stylexPlugin} from 'vite-plugin-stylex-dev';
 
 export default defineConfig(({mode}) => ({
-  base: '/',
+  base: '/sam2/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
